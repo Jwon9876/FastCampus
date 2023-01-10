ddcc69c1af85e030cb33a129c42a8e5e289c5fbe
Jwon <check4545@kakao.com>
2023-01-10 23:15:07 +0900

    Add UI

---
+++ Part3.Intermediate/SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  EnterEmailViewController.swift
+//  SpotifyLoginSampleApp
+//
+//  Created by 최주원 on 2023/01/10.
+//
+
+import UIKit
+
+class EnterEmailViewController: UIViewController{
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "이메일/비밀번호 입력하기"
+        
+        nextButton.layer.cornerRadius = 30
+        nextButton.isEnabled = false
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate  = self
+        
+        emailTextField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = false
+        
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        
+    }
+}
+
+extension EnterEmailViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let isEmailEmpty = emailTextField.text == ""
+        let isPasswordEmpty = passwordTextField.text == ""
+        
+        nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
+    }
+}
